941b489f124e2b9af53bf3ce9bd606281cbbc41c
Stefan Misik <stefan.misik@sap.com>
2019-11-17 22:49:26 +0100

    add comments to main script structure

--- FveScrollBot.py
+++ FveScrollBot.py
@@ -3,12 +3,16 @@ from lib.FveHttpClient import FveHttpClient
 
 
 def main():
+    # Initialize FVE ScrollBot
     url = 'http://192.168.2.51:8003/'
     print('Request starts at: {}'.format(url))
 
+    # Get Actual Measurements
     fve_http_client = FveHttpClient(url)
     fve_response = fve_http_client.get_actual_measurements()
     print(fve_response.text)
+
+    # Parse Measurements
 
 
 if __name__ == '__main__':
