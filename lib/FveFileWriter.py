30413f85e9c2441af64bf4de194767c9aee5942b
Stefan Misik <stefan.misik@sap.com>
2019-12-20 14:44:27 +0100

    definition for filewriter method to store day measurements

--- lib/FveFileWriter.py
+++ lib/FveFileWriter.py
@@ -6,6 +6,7 @@ class FveFileWriter:
     # constants
     SEMICOLON = ";"
     ACTUAL_MEASUREMENT_IDS = ['DaR', 'TiR', 'FE1', 'PPS', 'FP', 'Te', 'EL1', 'ETS', 'ETL']
+    DAY_MEASUREMENT_IDS = ['SSD1', 'SSD2', 'SSD3', 'SSD4', 'SSD5', 'SSD6', 'SSD7', 'SDS4', 'SDH4', 'SDL4', 'SDP4']
 
     # constructor
     def __init__(self, file):
@@ -39,3 +40,16 @@ class FveFileWriter:
 
         file.write(line + "\n")
         file.close()
+
+    # write day measurements into file
+    def write_day_measurement(self, day_measurement):
+        """
+            Write Day Measurement
+            1. <SSD{index}> - Date
+            2. <SDS4> - Surplus
+            3. <SDH4> - Consumed High Tariff
+            4. <SDL4> - Consumed Low Tariff
+            5. <SDP4> - Produced
+        :return: dictionary of all elements of the ROOT node
+        """
+        # TO-DO: finish implementation
