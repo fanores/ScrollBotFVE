78d51a1387e4939abc29030e3e72cc600a6f3102
Stefan Misik <stefan.misik@sap.com>
2019-12-20 14:44:54 +0100

    processing fve day measurements

--- FveScrollBot.py
+++ FveScrollBot.py
@@ -10,7 +10,9 @@ def main():
     url = 'http://192.168.2.51:8003/'
     print('Request starts at: {}'.format(url))
 
-    # Get Actual Measurements
+    # --------------------------------------------------------------------------------
+    # Get & Process Actual Measurements
+    # --------------------------------------------------------------------------------
     fve_http_client = FveHttpClient(url)
     fve_response = fve_http_client.get_actual_measurements()
     print(fve_response.text)
@@ -20,12 +22,29 @@ def main():
     actual_measurement = fve_xml_parser.parse_xml()
     print(actual_measurement)
 
-    # Write Measurements to File
+    # Write Actual Measurements to File
     project_folder_path = Path("measurements")
     actual_measurement_file = project_folder_path / "actual_measurement.txt"
     fve_file_writer = FveFileWriter(actual_measurement_file)
     fve_file_writer.write_actual_measurement(actual_measurement)
 
+    # --------------------------------------------------------------------------------
+    # Get & Process Daily Measurements
+    # --------------------------------------------------------------------------------
+    fve_http_client = FveHttpClient(url)
+    fve_response = fve_http_client.get_day_measurements(1)
+    print(fve_response.text)
+
+    # Parse Measurements
+    fve_xml_parser = FveXmlParser(fve_response.text)
+    day_measurement = fve_xml_parser.parse_xml()
+    print(day_measurement)
+
+    # Write Day Measurements to File
+    project_folder_path = Path("measurements")
+    day_measurement_file = project_folder_path / "day_measurement.txt"
+    fve_file_writer = FveFileWriter(day_measurement_file)
+    fve_file_writer.write_day_measurement(day_measurement)
 
 if __name__ == '__main__':
     main()
