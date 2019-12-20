bdedaa69beb3c62b140646899e82d0a2c448a160
Stefan Misik <stefan.misik@sap.com>
2019-12-20 14:29:47 +0100

    get daily measurements response

--- lib/FveHttpClient.py
+++ lib/FveHttpClient.py
@@ -33,3 +33,28 @@ class FveHttpClient:
             raise FveHttpError(print(error))
 
         return fve_response
+
+    # get daily measurements response body
+    def get_day_measurements(self, day):
+        """
+            Get Day Results -> 'http://192.168.2.51:8003/stat_day.xml?day={index}'
+            {index} - 1: yesterday
+                      2: day before yesterday
+                      ...
+            List of Elements:
+            -> <SSD{index}> : measured day
+            -> <SDS4>       : surplus (not consumed) by all phases L1 + L2 + L3
+            -> <SDH4>       : consumed by all phases L1 + L2 + L3 (high tarif)
+            -> <SDL4>       : consumed by all phases L1 + L2 + L3 (low tarif)
+            -> <SDP4>       : produced by all phases L1 + L2 + L3
+        :return: request response from FVE
+        """
+        request_url = self.url + self.DAY_MEASUREMENT_URL_SUFFIX
+        request_url.replace("{index}", day)
+
+        try:
+            fve_response = HttpClient.get_request(request_url)
+        except FveHttpError as error:
+            raise FveHttpError(print(error))
+
+        return fve_response
