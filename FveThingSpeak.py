7fe6518eeb158ab52b01638dbdf54dece9aaae0c
Stefan Misik <stefan.misik@sap.com>
2020-07-18 14:45:22 +0200

    upload measurement to thingspeak

---
+++ FveThingSpeak.py
@@ -0,0 +1,39 @@
+"""FVE ThingSpeak - Upload"""
+from lib.FveRestApi import FveRestApi
+from lib.FveXmlParser import FveXmlParser
+from lib.ThingSpeakRestApi import ThingSpeakRestApi
+from datetime import datetime
+
+
+def main():
+    # Initialize FVE
+    fve_url = 'http://192.168.2.51:8003/'
+    thing_speak_url = 'https://api.thingspeak.com/'
+    ts_api_key = '123'
+
+    # TODO: Schedule a job
+    job(fve_url, thing_speak_url, ts_api_key)
+
+
+def job(fve_url, ts_url, ts_api_key):
+    # --- START
+    print('Measurement starts at: {}'.format(datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
+
+    # --- FVE -> get day measurement
+    fve = FveRestApi(fve_url)
+    fve_response = fve.get_actual_measurements()
+
+    # --- PARSER -> retrieve measurement in a dictionary
+    xml_parser = FveXmlParser(fve_response.text)
+    measurement = xml_parser.parse_xml()
+
+    # --- THINGSPEAK -> upload measurement
+    thing_speak = ThingSpeakRestApi(ts_url, ts_api_key)
+    thing_speak.write_one_measurement_value(measurement.get('PPS', ""))
+
+    # --- END
+    print('Measurement is finished!')
+
+
+if __name__ == '__main__':
+    main()
