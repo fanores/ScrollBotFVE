a80ea3ffee9610d40645d283b4e4e6afd29c23b1
Stefan Misik <fanores@gmail.com>
2020-10-25 21:54:23 +0100

    examplePortraitClock fix indentation

--- scrollPhatHd/examplePortraitClock.py
+++ scrollPhatHd/examplePortraitClock.py
@@ -15,31 +15,31 @@ scrollphathd.set_brightness(0.3)
 scrollphathd.rotate(270)
 
 while True:
-    scrollphathd.clear()
+	scrollphathd.clear()
 
-    # See https://docs.python.org/2/library/time.html
-    # for more information on what the time formats below do.
+	# See https://docs.python.org/2/library/time.html
+	# for more information on what the time formats below do.
 
-    # Display the hour as two digits
-    scrollphathd.write_string(
-        time.strftime("%H"),
-        x=0,
-        y=0,
-        font=font5x5)
+	# Display the hour as two digits
+	scrollphathd.write_string(
+		time.strftime("%H"),
+		x=0,
+		y=0,
+		font=font5x5)
 
-    # Display the minute as two digits
-    scrollphathd.write_string(
-        time.strftime("%M"),
-        x=0,
-        y=6,
-        font=font5x5)
+	# Display the minute as two digits
+	scrollphathd.write_string(
+		time.strftime("%M"),
+		x=0,
+		y=6,
+		font=font5x5)
 
-    # Display the second as two digits
-    scrollphathd.write_string(
-        time.strftime("%S"),
-        x=0,
-        y=12,
-        font=font5x5)
+	# Display the second as two digits
+	scrollphathd.write_string(
+		time.strftime("%S"),
+		x=0,
+		y=12,
+		font=font5x5)
 
-    scrollphathd.show()
-    time.sleep(0.1)
+	scrollphathd.show()
+	time.sleep(0.1)
